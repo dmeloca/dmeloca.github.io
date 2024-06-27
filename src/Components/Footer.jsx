af0ba0589486b2d6b1b8dcdb28d293524c835466
Daniel Melo <115378192+dmeloca@users.noreply.github.com>
2024-06-27 22:59:54 +0000

    feat(main): add personal info

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -27,18 +27,7 @@ import youTubeIcon from "../images/socials/youtube.svg";
  */
 
 const Footer = (props) => {
-  const {
-    devDotTo,
-    email,
-    gitHub,
-    instagram,
-    linkedIn,
-    medium,
-    name,
-    primaryColor,
-    twitter,
-    youTube,
-  } = props;
+  const { devDotTo, email, gitHub, instagram, linkedIn, medium, name, twitter, youTube } = props;
 
   return (
     <div
@@ -49,8 +38,8 @@ const Footer = (props) => {
         alignItems: "center",
         gap: "2.5rem",
         padding: "5rem 0 3rem",
-        backgroundColor: primaryColor,
-        width: "100vw"
+        backgroundColor: "#702963",
+        width: "100vw",
       }}
     >
       <div
@@ -135,7 +124,6 @@ Footer.propTypes = {
   primaryColor: PropTypes.string,
   twitter: PropTypes.string,
   youTube: PropTypes.string,
-
 };
 
 export default Footer;
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -11,19 +11,6 @@
 import React from "react";
 
 /**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/design-desk.jpeg";
-
-const imageAltText = "desktop with books and laptop";
-
-/**
  * Project list
  *
  * An array of objects that will be used to display for your project
@@ -31,50 +18,47 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Bookshelf Management",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Bookshelf Manager is a simple Python script to manage your PDF book collection. It organizes books based on metadata embedded in the file names and allows you to search, list, and open books directly from the command line.",
+    url: "https://github.com/dmeloca/library",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Takemaru",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This code is an 'approximation' of a stenographer based on the python programming language, it has a cesar-mode cipher, and a tool to hide (and extract) messages in an image.",
+    url: "https://github.com/dmeloca/Takemaru",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "Yet another todo list",
+    description: "This is a simple command-line to-do list application written in Python.",
+    url: "https://github.com/dmeloca/to-do_list",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Investigation",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This repository serves as a unique platform dedicated to hosting investigations into topics relevant to the classes I've undertaken in my degree program. If you're interested in exploring these subjects further, feel free to peruse the content. Feedback is greatly appreciated as it helps me enhance my skills.",
+    url: "https://dmeloca.github.io/Investigation",
   },
 ];
 
+import image from "/src/images/black.png";
+
 const Portfolio = () => {
   return (
-    <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+    <section className="padding" id="portfolio" style={{}}>
+      <img className="background" src={image} alt="" />
+      <h2 style={{ textAlign: "center", color: "white" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
-            alt={imageAltText}
-          />
-        </div>
         <div className="container">
           {projectList.map((project) => (
             <div className="box" key={project.title}>
               <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+                <h3 style={{ flexBasis: "40px", color: "white" }}>{project.title}</h3>
               </a>
-              <p className="small">{project.description}</p>
+              <p className="small" style={{ color: "white" }}>
+                {project.description}
+              </p>
             </div>
           ))}
         </div>
